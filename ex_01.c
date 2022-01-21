be00ebd5e7cec31dabe4f8146f25939676375e66
Okonnor <loic.muller@epitech.eu>
2022-01-21 03:25:45 +0100

    ex 1 & 2

---
+++ ex_01.c
@@ -0,0 +1,36 @@
+/*
+** EPITECH PROJECT, 2022
+** Ines
+** File description:
+** ex_01.c
+*/
+
+
+
+/*
+ecrit un programme qui renvoie:
+1     si le nombre est pair
+0     si le nombre est impair
+84    si tu n'envoie pas un nombre
+
+ac -> le nombre "d'arguments"
+av -> les "arguments"
+
+exemples :
+./a.out   "Tu est un sorcier"  "Harry"
+  av[0]         av[1]           av[2]
+ac est donc égale a 3
+
+./a.out  comment est-ce   que    ca    va     Ines     ?
+  av[0]   av[1]  av[2]  av[3]  av[4] av[5]   av[6]   av[7]
+ac est donc égale a 8
+
+                //  ATTENTION   \\
+           les "arguments" sont des char *
+                  et non des int...
+*/
+
+int main(int ac, char **av)
+{
+    return 0;
+}
