f92b308c068901faed7f95609cdb43a51aba89b6
Okonnor <loic.muller@epitech.eu>
2022-04-18 18:57:59 +0200

    .

--- game_files/main.c
+++ game_files/main.c
@@ -9,12 +9,6 @@
 
 int main(void)
 {
-    mall_t *mal = NULL;
-    char *str = cl_strdup("hey, how are you? 42 WORds forty-two; fifty+one", &mal);
-    char **arr = cl_stwa(str, &mal);
 
-    arr = cl_sort_array(arr);
-    print_arr(arr);
-    cl_free(mal);
     return 0;
 }
--- info.sh
+++ info.sh
@@ -11,9 +11,11 @@ printf "\e[1;37m"
 $exe
 mkdir -p info
 find . -maxdepth 1 -executable -type f ! -name "*.sh" | ( read exe; xxd $exe > info/xxd.txt )
-find . -maxdepth 1 -executable -type f ! -name "*.sh" | ( read exe; strings $exe > info/strings.txt )
-find . -maxdepth 1 -executable -type f ! -name "*.sh" | ( read exe; readelf -S $exe > info/readelf.txt )
+# find . -maxdepth 1 -executable -type f ! -name "*.sh" | ( read exe; strings $exe > info/strings.txt )
+find . -maxdepth 1 -executable -type f ! -name "*.sh" | ( read exe; readelf -a $exe > info/readelf.txt )
 find . -maxdepth 1 -executable -type f ! -name "*.sh" | ( read exe; objdump -d $exe > info/objdump.txt )
+find . -maxdepth 1 -executable -type f ! -name "*.sh" | ( read exe; hexdump -d $exe > info/hexdump.txt )
+find . -maxdepth 1 -executable -type f ! -name "*.sh" | ( read exe; strace $exe 2> info/strace.txt 1> /dev/null)
 for file in ./info/*.txt
 do
     printf '\e[1;35m[ %s ]\e[1;37m has been created\n' "$file"
